632bf69b0c8d618d3b88506f982cadbc958c3dd3
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-12-09 17:59:37 +0800

    remove whole project

--- RESTBackend/rest_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import field
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['title', 'email', 'author']

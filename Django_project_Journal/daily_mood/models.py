5051f748218b122c849e11929b042e2dcf52d11a
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-12-15 00:23:28 +0800

    create models

--- Django_project_Journal/daily_mood/models.py
+++ Django_project_Journal/daily_mood/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.functions import Now
 
 # Create your models here.
+class timeline(models.Model):
+    #date_time = models.DateTimeField(db_default=Now())
+    date_added = models.DateTimeField(auto_now_add = True)
+    date_updated = models.DateTimeField(auto_now = True)
+    title = models.CharField(max_length = 255)
+    body = models.CharField(max_length = 255)
+    is_happy = models.BooleanField()
+    image = models.ImageField()
+    url = models.SlugField()

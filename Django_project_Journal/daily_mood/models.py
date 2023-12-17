c308ab2e3093f98de5502bbe116e6584b103cfdf
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-12-17 20:27:18 +0800

    finish first model

--- Django_project_Journal/daily_mood/models.py
+++ Django_project_Journal/daily_mood/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.db.models.functions import Now
 
 # Create your models here.
-class timeline(models.Model):
-    #date_time = models.DateTimeField(db_default=Now())
-    date_added = models.DateTimeField(auto_now_add = True)
-    date_updated = models.DateTimeField(auto_now = True)
-    title = models.CharField(max_length = 255)
-    body = models.CharField(max_length = 255)
-    is_happy = models.BooleanField()
-    image = models.ImageField()
-    url = models.SlugField()
+class Emp(models.Model):
+    name = models.CharField(max_length = 255)
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to = 'images')
+    email_address = models.EmailField(max_length = 100, unique = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class items(models.Model):
+    name = models.CharField(max_length = 255)
+    
--- Django_project_Journal/daily_mood/views.py
+++ Django_project_Journal/daily_mood/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Emp
 
 # Create your views here.
+def home(request):
+    e = Emp.objects.all()
+    context = {
+        "emp":e
+    }
+    return render(request, 'homePage.html',context)

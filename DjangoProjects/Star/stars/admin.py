af94cadd9f07a736c071352502aa03185b9fda0a
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2024-01-01 01:29:45 +0800

    update

--- DjangoProjects/Star/stars/admin.py
+++ DjangoProjects/Star/stars/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tasks
+from .models import Tasks,Todos
 
 # Register your models here.
 admin.site.register(Tasks)
+admin.site.register(Todos)
--- DjangoProjects/Star/stars/models.py
+++ DjangoProjects/Star/stars/models.py
@@ -1,4 +1,4 @@
-from email.policy import default
+
 from django.db import models
 
 # Create your models here.
@@ -7,3 +7,12 @@ class Tasks(models.Model):
     number = models.IntegerField()
     isStar = models.BooleanField(default='false')
     image = models.ImageField(upload_to='images', null=True, blank=True)
+
+class Todos(models.Model):
+    task = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.task
